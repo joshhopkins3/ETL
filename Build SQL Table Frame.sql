f556e9f55e7c6d0c0b68a98edd5dc02ec848d878
rbvancleave <48543007+rbvancleave@users.noreply.github.com>
2021-06-01 20:04:12 -0500

    SQL connection established and DB updated

---
+++ Build SQL Table Frame.sql
@@ -0,0 +1,11 @@
+CREATE TABLE nba_df ("Date" DATE, "VH" VARCHAR,"Team" VARCHAR, "Final" INT, "ML" DECIMAL
+);
+
+CREATE TABLE nhl_df ("Date" DATE, "VH" VARCHAR,"Team" VARCHAR, "Final" INT, "ML" DECIMAL
+);
+
+CREATE TABLE nfl_df ("Date" DATE, "VH" VARCHAR,"Team" VARCHAR, "Final" INT, "ML" DECIMAL
+);
+
+CREATE TABLE mlb_df ("Date" DATE, "VH" VARCHAR,"Team" VARCHAR, "Final" INT, "ML" DECIMAL
+);
